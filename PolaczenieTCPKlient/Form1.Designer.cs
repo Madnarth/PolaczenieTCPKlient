ddd0e962c4a13d6dafd4c236629ab502863993c1
Madnarth <corwyn.blade@gmail.com>
2018-03-20 20:55:06 +0100

    Initial commit

---
+++ PolaczenieTCPKlient/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PolaczenieTCPKlient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbHostAddress = new System.Windows.Forms.TextBox();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.bConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbHostAddress
+            // 
+            this.tbHostAddress.Location = new System.Drawing.Point(54, 27);
+            this.tbHostAddress.Name = "tbHostAddress";
+            this.tbHostAddress.Size = new System.Drawing.Size(211, 20);
+            this.tbHostAddress.TabIndex = 0;
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(306, 27);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(66, 20);
+            this.numPort.TabIndex = 1;
+            // 
+            // lbLogs
+            // 
+            this.lbLogs.FormattingEnabled = true;
+            this.lbLogs.Location = new System.Drawing.Point(12, 75);
+            this.lbLogs.Name = "lbLogs";
+            this.lbLogs.Size = new System.Drawing.Size(360, 95);
+            this.lbLogs.TabIndex = 2;
+            // 
+            // bConnect
+            // 
+            this.bConnect.Location = new System.Drawing.Point(155, 176);
+            this.bConnect.Name = "bConnect";
+            this.bConnect.Size = new System.Drawing.Size(75, 23);
+            this.bConnect.TabIndex = 3;
+            this.bConnect.Text = "Połącz";
+            this.bConnect.UseVisualStyleBackColor = true;
+            this.bConnect.Click += new System.EventHandler(this.bConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adres:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bConnect);
+            this.Controls.Add(this.lbLogs);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.tbHostAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbHostAddress;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.ListBox lbLogs;
+        private System.Windows.Forms.Button bConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PolaczenieTCPKlient/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolaczenieTCPKlient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            string host = tbHostAddress.Text;
+            int port = System.Convert.ToInt16(numPort.Value);
+            try
+            {
+                TcpClient klient = new TcpClient(host, port);
+                lbLogs.Items.Add("Nawiązano połączenie z " + host + " na porcie: " + port);
+               
+                klient.Close();
+            }
+            catch (Exception ex)
+            {
+                lbLogs.Items.Add("Błąd: Nie udało się nawiązać połączenia!");
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
